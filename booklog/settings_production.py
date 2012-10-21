12e032396db94c8b502975ae3cb2dab4969dc0e6
Austin Platt <austinplatt@gmail.com>
2012-10-21 16:16:09 +0100

    added static to production settings

--- booklog/settings_production.py
+++ booklog/settings_production.py
@@ -12,3 +12,10 @@ DATABASES = {
          'PORT' : '',
      }
 }
+
+STATICFILES_DIRS = (
+    '/home/ubuntu/booklog/static',
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
